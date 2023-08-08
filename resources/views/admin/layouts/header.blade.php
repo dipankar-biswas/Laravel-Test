1f2cf9c2fc0548456ec086c25c1b9edae220ea65
Dipankar Biswas <84431857+themedesigner3@users.noreply.github.com>
2023-08-08 12:17:43 +0600

    first commit

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<header class="layout-header">
+    <div class="header-content">
+        <div class="left">
+            <div class="search icon">
+                <i class="fa-light fa-magnifying-glass"></i>
+            </div>
+        </div>
+        <div class="right">
+            <div class="language icon">
+                <i class="fa-thin fa-language"></i>
+            </div>
+            <div class="notification icon">
+                <i class="fa-light fa-bell"></i>
+            </div>
+            <div class="avatar">
+                <img src="assets/images/user.png" alt="Avatar">
+            </div>
+        </div>
+    </div>
+</header>
