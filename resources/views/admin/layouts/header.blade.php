0c228dcefc64bc3ef124d362940a6a7064eb8b58
Dipankar Biswas <84431857+themedesigner3@users.noreply.github.com>
2023-08-08 16:07:58 +0600

    update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -2,15 +2,15 @@
     <div class="header-content">
         <div class="left">
             <div class="search icon">
-                <i class="fa-light fa-magnifying-glass"></i>
+                <i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
         <div class="right">
             <div class="language icon">
-                <i class="fa-thin fa-language"></i>
+                <i class="fa-solid fa-language"></i>
             </div>
             <div class="notification icon">
-                <i class="fa-light fa-bell"></i>
+                <i class="fa-regular fa-bell"></i>
             </div>
             <div class="avatar">
                 <img src="assets/images/user.png" alt="Avatar">
